a934acccff40e314f60ecd523c9d13a9088f4e91
yewinthlaing04 <yewinthlaing23@gmail.com>
2025-03-18 22:17:56 +0100

    deleting all files

--- NeoBank-Cloud-Config/account/src/main/java/com/ye/account/service/ICustomerService.java
+++
@@ -1,8 +0,0 @@
-package com.ye.account.service;
-
-import com.ye.account.dto.CustomerDetailsDto;
-
-public interface ICustomerService {
-
-    CustomerDetailsDto fetchCustomerDetails(String mobileNumber , String correlationId);
-}
