15edc6ba097f23de2e403c1142abfa46b4641c9d
yewinthlaing04 <yewinthlaing23@gmail.com>
2025-03-23 21:17:17 +0100

    gateway server implementation

--- SpringMicroservice/gatewayserver/src/main/java/com/ye/gatewayserver/GatewayserverApplication.java
+++ SpringMicroservice/gatewayserver/src/main/java/com/ye/gatewayserver/GatewayserverApplication.java
@@ -23,7 +23,8 @@ public class GatewayserverApplication {
                 .route(p -> p
                         .path("/neobank/accounts/**")
                         .filters( f -> f.rewritePath("/neobank/accounts/(?<segment>.*)","/${segment}")
-                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(config -> config.setName("accountsCircuitBreaker")))
                         .uri("lb://ACCOUNTS"))
                 .route(p -> p
                         .path("/neobank/loans/**")
